0a1c4742fa970ca8b0a0ac3144c26740c5b7cd92
Artem Turchyn <Artem.at131@gmail.com>
2026-06-23 23:07:00 +0300

    Configure and optimize Django admin site for all models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Position, TaskType, Task, Worker
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = [
+        "name",
+        "task_type__name",
+    ]
+    list_display = [
+        "name",
+        "task_type",
+        "deadline",
+        "priority",
+        "get_assignees"
+    ]
+    list_filter = ["deadline", "priority", "task_type"]
+
+    def get_assignees(self, obj):
+        return ", ".join([worker.username for worker in obj.assignees.all()])
+
+    get_assignees.short_description = "Assignees"
+
+    def get_queryset(self, request):
+        qs = super(TaskAdmin, self).get_queryset(request)
+        return qs.select_related("task_type").prefetch_related("assignees")
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    search_fields = ["username", "position__name"]
+    list_display = [
+        "username",
+        "position",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    ]
+    list_filter = ["position", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (("Additional info:",
+                                        {"fields": ("position",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info:",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "position",
+                )
+            },
+        ),
+    )
+
+    def get_queryset(self, request):
+        qs = super(WorkerAdmin, self).get_queryset(request)
+        return qs.select_related("position")
