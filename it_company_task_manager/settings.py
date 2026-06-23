fa5cab958a174a6d1cae256db06f802917ca3885
Artem Turchyn <Artem.at131@gmail.com>
2026-06-23 20:41:03 +0300

    add Woker as AUTH_USER_MODEL

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.auth.models import AbstractUser
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "tasks.Worker"
