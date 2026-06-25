e57f1c4b63870a387b120ee2d41a165fb5d49ad3
Artem Turchyn <Artem.at131@gmail.com>
2026-06-25 21:43:04 +0300

    Add forms for Task, Worker, and search functionality

---
+++ tasks/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from tasks.models import Task, Worker
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "assignees": forms.CheckboxSelectMultiple(),
+            "deadline": forms.DateInput(attrs={"type": "date"}),
+        }
+
+
+class WorkerForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = ("username", "email", "first_name", "last_name", "position")
+
+
+class TaskNameSearchForm(forms.Form):
+    name = forms.CharField(max_length=255, required=False)
+
+
+class WorkerUsernameSearchForm(forms.Form):
+    username = forms.CharField(max_length=255, required=False)
