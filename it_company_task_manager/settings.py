cb918613be0ed905c182aaa75866fdbfb46d5ae6
Artem Turchyn <Artem.at131@gmail.com>
2026-06-26 23:43:41 +0300

    config: set up global templates, static files

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -118,5 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = "tasks.Worker"
