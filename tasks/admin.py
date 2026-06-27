e9ff2bfb98f5f651f15fb28860d27e1280bc3f61
Artem Turchyn <Artem.at131@gmail.com>
2026-06-27 14:13:25 +0300

    add tags in task admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -24,24 +24,29 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = [
         "name",
         "task_type__name",
+        "tags__name",
     ]
     list_display = [
         "name",
         "task_type",
+        "get_tags",
         "deadline",
         "priority",
         "get_assignees"
     ]
-    list_filter = ["deadline", "priority", "task_type"]
+    list_filter = ["deadline", "priority", "task_type", "tags"]
 
     def get_assignees(self, obj):
         return ", ".join([worker.username for worker in obj.assignees.all()])
-
     get_assignees.short_description = "Assignees"
+
+    def get_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    get_tags.short_description = "Tags"
 
     def get_queryset(self, request):
         qs = super(TaskAdmin, self).get_queryset(request)
-        return qs.select_related("task_type").prefetch_related("assignees")
+        return qs.select_related("task_type").prefetch_related("assignees", "tags")
 
 
 @admin.register(Worker)
