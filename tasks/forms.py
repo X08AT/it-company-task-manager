bd4d35ab7ee199fc1253f09e453d88bcf3d71b9d
Artem Turchyn <Artem.at131@gmail.com>
2026-06-27 12:06:08 +0300

    fix TaskCreateForm, improve search forms and Worker form

--- tasks/forms.py
+++ tasks/forms.py
@@ -14,24 +14,36 @@ class TaskUpdateForm(forms.ModelForm):
         }
 
 
-class TaskCreateForm(TaskUpdateForm):
-    class Meta(TaskUpdateForm.Meta):
+class TaskCreateForm(forms.ModelForm):
+    class Meta:
+        model = Task
         exclude = ["is_completed"]
+        widgets = {
+            "assignees": forms.CheckboxSelectMultiple(),
+            "deadline": forms.DateInput(attrs={"type": "date"}),
+        }
 
 
 class WorkerForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-
     class Meta(UserCreationForm.Meta):
         model = Worker
-        fields = ("username", "email", "first_name", "last_name", "position")
+        fields = UserCreationForm.Meta.fields + (
+            "email",
+            "first_name",
+            "last_name",
+            "position"
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].required = True
+        self.fields["last_name"].required = True
+        self.fields["email"].required = True
 
 
 class TaskNameSearchForm(forms.Form):
-    name = forms.CharField(max_length=255, required=False)
+    name = forms.CharField(max_length=255, required=False, label="", widget=forms.TextInput(attrs={"placeholder": "Search tasks by name..."}))
 
 
 class WorkerUsernameSearchForm(forms.Form):
-    username = forms.CharField(max_length=255, required=False)
+    username = forms.CharField(max_length=255, required=False, label="", widget=forms.TextInput(attrs={"placeholder": "Search worker by username..."}))
