06b4332a10fdaac477a12ebfdf896f42785abcc3
Artem Turchyn <Artem.at131@gmail.com>
2026-06-29 17:48:10 +0300

    set DEBUG True

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f#@5n_nwob-d0&+z2ki%rn7bn)w$uxr@_a_i4a)ap6)c#gg-cw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
